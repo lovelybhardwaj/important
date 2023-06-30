989a348d06ad6f7745c2d14c6527bbf19107f6dd
lovelybhardwaj <131432850+lovelybhardwaj@users.noreply.github.com>
2023-06-30 17:56:05 +0530

    Update networks.py

--- model/networks.py
+++ model/networks.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch.nn import init
 import functools
 from torch.optim import lr_scheduler
-from model.util import *
+from important.model.util import *
 
 ###############################################################################
 # Helper Functions
